1964e31e1f1b83b74c3701674a3f21953bdd3505
kenas <kenasbut@gmail.com>
2025-01-07 15:02:22 +0200

    Pridėtas biudzetas.py

--- models/app.py
+++ models/app.py
@@ -1,2 +1,17 @@
-biudzetas = biudzetas()
+from biudzetas import Biudzetas
 
+biudzetas = Biudzetas()
+
+while True:
+    choice = int(input("1 - Peržiūrėti\n2 - Įvesti pajamas\n3 - Įvesti išlaidas\n4 - Balansas\n0 - Išeiti\n"))
+    match choice:
+        case 1:
+            print(biudzetas.zurnalas)
+        case 2:
+            biudzetas.prideti_pajamas()
+        case 3:
+            biudzetas.prideti_islaidas()
+        case 4:
+            print("Balansas", biudzetas.gauti_balansa())
+        case 0:
+            break
